af0d4763ab8cdeb3711a85b1366db3dc123d7787
EricksonLopes <ofc.erickson@gmail.com>
2020-08-31 10:49:47 -0300

    Utilizando métodos, biblioteca Request e JSON

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
 
 
@@ -9,14 +9,20 @@ def pessoas(pk):
     return jsonify({'pk': pk, 'nome': 'Erickson', 'profissao': 'Carpinteiro'})
 
 
-@app.route('/soma/<int:v1>/<int:v2>')
-def soma(v1, v2):
-    return {'soma': v1 + v2}
+# @app.route('/soma/<int:v1>/<int:v2>')
+# def soma(v1, v2):
+#     return {'soma': v1 + v2}
 
 
-@app.route('/soma/', methods=['POST'])
+@app.route('/soma/', methods=['POST', 'GET'])
 def soma_post():
-    return jsonify({'soma': 'soma'})
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        soma = sum(dados['valores'])
+        return jsonify({'soma': soma})
+
+    elif request.method == 'GET':
+        return jsonify({'message': 'Some os valores enviado como método post'})
 
 
 if __name__ == '__main__':
--- request.py
+++ request.py
@@ -1,5 +1,8 @@
 import requests
 
-post = requests.post('http://127.0.0.1:5000/soma/')
+post = requests.post('http://127.0.0.1:5000/soma/', json={'valores': [10, 40, 10]})
+get = requests.get('http://127.0.0.1:5000/soma/')
 
+print(get.json())
 print(post.json())
+
