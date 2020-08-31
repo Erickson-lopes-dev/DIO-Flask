cb8f89ec9dbcc35ee891302271fd19f0b4b14e79
EricksonLopes <ofc.erickson@gmail.com>
2020-08-31 10:19:12 -0300

    retornando json

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
-def ola():
-    return 'ola mundo'
+@app.route('/')
+def pessoas():
+    return {'nome': 'Erickson'}
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
